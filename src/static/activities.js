b0ee1a9ca81ee8440401a3945ead2c668383dff0
chengpengzhao <z609722559@gmail.com>
2020-11-14 12:51:23 +0000

    Add new runs

--- src/static/activities.js
+++ src/static/activities.js
@@ -12,6 +12,20 @@ const activities = [
     "average_heartrate": null,
     "average_speed": 2.825492131575635,
     "streak": 1
+  },
+  {
+    "run_id": 1605341171878,
+    "name": "run from gpx",
+    "distance": 4770.061609779714,
+    "moving_time": "0:28:47.121000",
+    "type": "Run",
+    "start_date": "2020-11-14 08:06:11",
+    "start_date_local": "2020-11-14 16:06:11",
+    "location_country": "\u4e2d\u56fd\u822a\u5929\u7a7a\u6c14\u52a8\u529b\u6280\u672f\u7814\u7a76\u9662, \u5357\u5bab\u6ee8\u6cb3\u8def, \u4e30\u53f0\u533a, \u5317\u4eac\u5e02, China \u4e2d\u56fd",
+    "summary_polyline": "sq~qFkzycUWVS_GiCl@g@nBVtJqIl@kJvWg@rCjKc[DeBgAe@aBVqAgBg@r@sFPqA_Tb@uGWc@iChCeF~IN|MuBvAVpJYtEiDd@jGd@`HKJj@Q_Ok@wDRm@xD?@m@xHZ^{AGkCjNgAdAaCtBLRdB[P",
+    "average_heartrate": null,
+    "average_speed": 2.7618572235412073,
+    "streak": 1
   }
 ];
 
